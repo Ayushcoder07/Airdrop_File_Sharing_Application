0359c9844e7f9afeaad40b6e1b2a4dc156972b71
Ayushcoder07 <108988850+Ayushcoder07@users.noreply.github.com>
2025-05-24 12:15:22 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("com\\..*")
                 includeGroupByRegex("androidx.*")
             }
         }
